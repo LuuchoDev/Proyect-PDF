a23939500c0a4373507011e55f79e7a2504a764d
Sosa, Luciano Emanuel <emanuelsosa00@hotmail.com>
2025-11-18 10:39:53 -0300

    Add Flask app to convert images to PDF
    Introduces a Flask backend with endpoints to render the main page and generate a PDF from uploaded images. Includes image processing and error handling logic. Adds an empty index.html template as a placeholder for the frontend.

---
+++ app.py
@@ -0,0 +1,62 @@
+import os
+from flask import flash, render_template, request, send_file
+from PIL import Image
+import io
+
+app = Flask(__name__)
+
+def index():
+    """
+    Esta función carga la página web principal (nuestro frontend).
+    Flask buscará 'index.html' en la carpeta 'templates'.
+    """
+    return render_template('index.html')
+
+def generar_pdf():
+    """
+    Esta función genera un archivo PDF a partir de una imagen subida por el usuario.
+    Esta es la función principal del backend.
+    Se activa cuando el JavaScript le envía las imágenes.
+    """
+    lista_imagenes_procesadas = []
+    archivos_subidos = request.files.getlist('images')
+
+    if not archivos_subidos():
+        return "No se han subido imágenes.", 400
+    
+    try:
+        for archivo in archivos_subidos:
+            img = Image.open(archivo.stream)
+
+            ## Aseguramos que este en modo RGB
+            if img.mode == "RGBA":
+                img = img.convert("RGB")
+            
+            lista_imagenes_procesadas.append(img)
+        
+        # Guardar las imágenes en un archivo PDF en memoria
+        if lista_imagenes_procesadas:
+            pdf_buffer = io.BytesIO()
+            lista_imagenes_procesadas[0].save(
+                pdf_buffer,
+                format='PDF',
+                save_all=True,
+                append_images=lista_imagenes_procesadas[1:]
+            )
+            pdf_buffer.seek(0)
+
+            # Enviar el archivo PDF generado al usuario
+            return send_file(
+                pdf_buffer,
+                as_attachment=True,
+                download_name="mi_pdf.pdf",
+                mimetype='application/pdf'
+            )
+    except Exception as e:
+        ## Manejo básico de errores
+        print(f"Error: {e}")
+        return f"Error al procesar las imágenes.", 500
+
+## Iniciar aplicación
+if __name__ == '__main__':
+    app.run(debug=True)
